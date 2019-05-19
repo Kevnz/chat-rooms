5816e717638e51189b128d2cd0b175e57de0e96f
Kevin Isom <kevin.isom@gmail.com>
2019-05-19 14:55:56 +1200

    no logging

--- src/server/services/cache.js
+++ src/server/services/cache.js
@@ -3,15 +3,9 @@ const cache = new Cache()
 
 module.exports = {
   push: async (key, item) => {
-    console.info(cache)
     cache.redis.lpush(key, JSON.stringify(item))
   },
-  getHistory: async key => {
-    console.info(cache)
-    const history = cache.redis.lrange(key, 0, -1)
-    console.info(key, history)
-    return JSON.parse(history)
-  },
+  getHistory: async key => cache.redis.lrange(key, 0, -1),
   get: async (key, getter) => {
     const fromCache = await cache.get(key)
     if (!fromCache) {
