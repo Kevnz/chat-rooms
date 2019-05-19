53c55106a9ad745135e7e8f2ed502bd84a5f5d67
Kevin Isom <kevin.isom@gmail.com>
2019-05-20 09:21:40 +1200

    index

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,10 +1,6 @@
+import './rhlConfig'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './core/app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-if (process.env.NODE_ENV !== 'production') {
-  module.hot.accept()
-}
-
