c5a8d2ac00297e1ce65d8fb9605748a0576b6bf0
Kevin Isom <kevin.isom@gmail.com>
2019-05-20 08:50:01 +1200

    app

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -1,24 +1,37 @@
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 import { Router } from '@reach/router'
+import { hot } from 'react-hot-loader/root'
+import { Layout } from './layout'
+import { AuthProvider } from './context/auth'
+import { ErrorBoundary } from './boundary'
 
-
-const About = React.lazy(() => import('../features/about'))
-const Home = React.lazy(() => import('../features/home'))
-const Contact = React.lazy(() => import('../features/contact'))
-
-export default class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <h1>App</h1>
-        <React.Suspense fallback={<div>Loading</div>}>
-          <Router>
-            <Home path="/" />
-            <About path="/about" />
-            <Contact path="/contact" />
-          </Router>
-        </React.Suspense>
-      </Fragment>
-    )
-  }
+const About = React.lazy(() => import('../pages/about'))
+const Home = React.lazy(() => import('../pages/home'))
+const Contact = React.lazy(() => import('../pages/contact'))
+const Register = React.lazy(() => import('../pages/register'))
+const Login = React.lazy(() => import('../pages/login'))
+const Room = React.lazy(() => import('../pages/room'))
+export const App = () => {
+  return (
+    <ErrorBoundary>
+      <AuthProvider>
+        <Layout>
+          <ErrorBoundary>
+            <React.Suspense fallback={<div>Loading</div>}>
+              <Router>
+                <Home path="/" />
+                <Room path="/rooms/:slug" />
+                <About path="/about" />
+                <Contact path="/contact" />
+                <Register path="/register" />
+                <Login path="/login" />
+              </Router>
+            </React.Suspense>
+          </ErrorBoundary>
+        </Layout>
+      </AuthProvider>
+    </ErrorBoundary>
+  )
 }
+
+export default hot(App)
