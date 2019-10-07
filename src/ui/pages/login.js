c908f1194fb1389a8875e68a2fdc51edea6bba9b
Kevin Isom <kevin.isom@gmail.com>
2019-10-07 22:46:10 +1300

    login page

--- src/ui/pages/login.js
+++ src/ui/pages/login.js
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Form, TextBox, Password, Button } from 'react-form-elements'
-import gql from 'graphql-tag'
 import { navigate } from '@reach/router'
+import { Container, Section } from '@brightleaf/elements'
 import { useMutation } from '../hooks/use-mutation'
 import { AuthContext } from '../core/context/auth'
 
-const LOGIN_MUTATION = gql`
+const LOGIN_MUTATION = `
   mutation Login($loginInput: LoginInput!) {
     login(loginInput: $loginInput) {
       token
@@ -21,38 +21,59 @@ const LOGIN_MUTATION = gql`
 `
 
 export const LoginPage = () => {
-  const loginUser = useMutation(LOGIN_MUTATION)
+  const { loading, error, data, complete, makeQuery: loginUser } = useMutation(
+    '/graphql',
+    LOGIN_MUTATION
+  )
+  console.log('loading', { loading, error, data, complete })
   const { state, dispatch } = useContext(AuthContext)
 
   if (state.isLoggedIn) {
-    return <div>Already logged in</div>
+    navigate('/', {
+      message: 'Logged In',
+    })
   }
-  return (
-    <main>
-      <Form
-        name="loginForm"
-        onSubmit={formData => {
-          loginUser({
-            update: (proxy, mutationResult) => {
-              console.info('prox', proxy)
-              console.info('mutie', mutationResult)
-              const { token, user } = mutationResult.data.login
+  useEffect(() => {
+    console.log('use eff')
+    if (complete && data && data.login) {
+      const { token, user } = data.login
 
-              dispatch({
-                type: 'login',
-                payload: { token, user },
-              })
-              navigate('/')
-            },
-            variables: { loginInput: formData },
-          })
-        }}
-      >
-        <TextBox type="email" label="Email" name="email" />
-        <Password label="Password" name="password" />
-        <Button>Login</Button>
-      </Form>
-    </main>
+      dispatch({
+        type: 'login',
+        payload: { token, user },
+      })
+    }
+  }, [complete, data])
+  console.log('complete && data ', complete, data)
+  return (
+    <Container>
+      <Section>
+        <Form
+          className="form"
+          name="loginForm"
+          onSubmit={formData => {
+            loginUser({ loginInput: formData })
+          }}
+        >
+          <TextBox
+            type="email"
+            label="Email"
+            name="email"
+            className="field control"
+            inputClassName="input"
+            labelClassName="label"
+          />
+          <Password
+            label="Password"
+            name="password"
+            className="field control"
+            inputClassName="input"
+            labelClassName="label"
+          />
+          <Button>Login</Button>
+        </Form>
+      </Section>
+    </Container>
   )
 }
 
