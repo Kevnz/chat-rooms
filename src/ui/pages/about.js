dcca74fabecf1de4712fdaf754a0011501e17009
Kevin Isom <kevin.isom@gmail.com>
2019-10-07 22:45:51 +1300

    about page

--- src/ui/pages/about.js
+++ src/ui/pages/about.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
-export default () => (
-  <main>
-    <Helmet>
-      <title>About The Conclave</title>
-    </Helmet>
-    <h1>About</h1>
-  </main>
-)
+import {
+  Container,
+  Section,
+  Hero,
+  HeroBody,
+  Title,
+  SubTitle,
+} from '@brightleaf/elements'
+import { useTitle } from '@brightleaf/react-hooks'
+export default () => {
+  useTitle('About Chat Rooms')
+  return (
+    <Container>
+      <Hero>
+        <HeroBody>
+          <Title>Chat-Rooms</Title>
+          <SubTitle>Chatting on the web</SubTitle>
+        </HeroBody>
+      </Hero>
+      <Section>Put something here</Section>
+    </Container>
+  )
+}
