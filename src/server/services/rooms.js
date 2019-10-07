b7110b443f1bbb75547ac9e77c7dab39da432a9f
Kevin Isom <kevin.isom@gmail.com>
2019-10-07 22:43:29 +1300

    room service

--- src/server/services/rooms.js
+++ src/server/services/rooms.js
@@ -1,4 +1,5 @@
-const { Room } = require('../models')
+const { mapper } = require('@kev_nz/async-tools')
+const { Room, User } = require('../models')
 const cache = require('./cache')
 
 module.exports = {
@@ -15,7 +16,24 @@ module.exports = {
   },
   getMessages: async room => {
     const messages = await cache.getHistory(room)
-    return messages.map(m => JSON.parse(m))
+    console.log('messages???', messages)
+    const mapped = messages.map(m => JSON.parse(m))
+    console.log('mapped???', mapped)
+    const full = await mapper(mapped, async message => {
+      console.log('message', message)
+      try {
+        const user = await new User({ username: message.user }).fetch({})
+        message.fullUser = user.toJSON({
+          hidden: ['firstName', 'lastName', 'email', 'password'],
+        })
+        return message
+      } catch (err) {
+        console.error(err)
+        return message
+      }
+    })
+    console.log('full', full)
+    return full
   },
   getAll: async () => {
     return cache.get('all-rooms', async () => {
