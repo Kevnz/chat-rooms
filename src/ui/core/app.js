c4d485b68e9de2141596a2d9ad9fb27efb170231
Kevin Isom <kevin.isom@gmail.com>
2019-10-07 22:44:07 +1300

    app

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -3,7 +3,7 @@ import { Router } from '@reach/router'
 import { hot } from 'react-hot-loader/root'
 import { Layout } from './layout'
 import { AuthProvider } from './context/auth'
-import { ErrorBoundary } from './boundary'
+import { RoomsProvider } from './context/rooms'
 
 const About = React.lazy(() => import('../pages/about'))
 const Home = React.lazy(() => import('../pages/home'))
@@ -13,24 +13,23 @@ const Login = React.lazy(() => import('../pages/login'))
 const Room = React.lazy(() => import('../pages/room'))
 export const App = () => {
   return (
-    <ErrorBoundary>
-      <AuthProvider>
+    <AuthProvider>
+      <RoomsProvider>
         <Layout>
-          <ErrorBoundary>
-            <React.Suspense fallback={<div>Loading</div>}>
-              <Router>
-                <Home path="/" />
-                <Room path="/rooms/:slug" />
-                <About path="/about" />
-                <Contact path="/contact" />
-                <Register path="/register" />
-                <Login path="/login" />
-              </Router>
-            </React.Suspense>
-          </ErrorBoundary>
+          <React.Suspense fallback={<div>Loading</div>}>
+            <Router>
+              <Home path="/" />
+              <Login path="/login" />
+              <Room path="/rooms/:slug" />
+              <About path="/about" />
+              <Contact path="/contact" />
+              <Register path="/register" />
+              <Login path="/login" />
+            </Router>
+          </React.Suspense>
         </Layout>
-      </AuthProvider>
-    </ErrorBoundary>
+      </RoomsProvider>
+    </AuthProvider>
   )
 }
 
