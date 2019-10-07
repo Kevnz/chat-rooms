2afa22ca90318bc1655de1275f93d4f8775ba33e
Kevin Isom <kevin.isom@gmail.com>
2019-10-07 22:46:39 +1300

    generate fixtures

--- src/utils/fixtures.js
+++ src/utils/fixtures.js
@@ -9,17 +9,32 @@ const rooms = []
 for (let i = 1; i < 51; i++) {
   users.push(userGenerator(i))
 }
+
+users.push({
+  id: 51,
+  firstName: 'Kevin',
+  lastName: 'Isom',
+  username: 'the_kev',
+  email: 'kevin_isom@the-kev.com',
+  password: 'whDhnNBcOjdZVxa',
+  hashedPassword:
+    '$2b$10$pJEslmCtekRfq0QgeqqFuOksHMw4k5fe/OIyTMtK/ZqQwGIkpX8Uq',
+  avatar: 'https://api.adorable.io/avatars/face/eyes1/nose3/mouth2/00abff',
+})
+
 rooms.push({
   id: 1,
   name: 'General',
   description: 'General Room',
   slug: 'general',
+  icon: 'inbox',
 })
 rooms.push({
   id: 2,
   name: 'Random',
   description: 'Random Talk Room',
   slug: 'random',
+  icon: 'random',
 })
 for (let i = 3; i < 11; i++) {
   rooms.push(roomGenerator(i))
