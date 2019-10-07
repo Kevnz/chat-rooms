5bbe9a2966cfffd50e11402bd3799bb937c8b114
Kevin Isom <kevin.isom@gmail.com>
2019-10-07 22:46:54 +1300

    generators

--- src/utils/generators/rooms.js
+++ src/utils/generators/rooms.js
@@ -1,11 +1,33 @@
 const faker = require('faker')
 
+const icons = [
+  'inbox',
+  'cocktail',
+  'coffee',
+  'flask',
+  'highlighter',
+  'layer-group',
+  'tint',
+  'calendar',
+  'eye',
+  'football-ball',
+  'quidditch',
+  'volleyball-ball',
+  'radiation',
+  'atom',
+]
+
 module.exports = id => {
-  const roomName = faker.company.catchPhrase()
+  const roomName = faker.commerce.productName()
+  const roomArr = roomName.split(' ')
+  roomArr.pop()
+  const icon = icons[Math.floor(Math.random() * icons.length)]
+
   return {
     id: id,
-    name: roomName,
+    name: roomArr.join(' '),
     description: faker.company.bs(),
     slug: faker.helpers.slugify(roomName).toLowerCase(),
+    icon: icon,
   }
 }
--- src/utils/generators/users.js
+++ src/utils/generators/users.js
@@ -8,16 +8,18 @@ module.exports = (id, role = 'user') => {
   const hashedPassword = bcrypt.hashSync(password, salt)
   const firstName = faker.name.firstName()
   const lastName = faker.name.lastName()
+  const username = faker.internet.userName(firstName, lastName).toLowerCase()
 
   return {
     id: id,
     firstName,
     lastName,
-    username: faker.internet.userName(firstName, lastName).toLowerCase(),
+    username,
     email: faker.internet
       .email(firstName, lastName, 'example.com')
       .toLowerCase(),
     password,
     hashedPassword,
+    avatar: `https://api.adorable.io/avatars/120/${username}.png`,
   }
 }
